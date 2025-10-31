6cc2d190936d2d7491115b8c659eb3132bae9bc8
tahiryyvon <tahiry.yvon@gmail.com>
2025-10-31 12:35:20 +0300

    add test

--- api/index.js
+++ api/index.js
@@ -1,23 +1,27 @@
 // api/index.js
-const { NestFactory } = require('@nestjs/core');
-const { AppModule } = require('../backend/dist/app.module');
 const express = require('express');
 
 let app;
 
-async function bootstrap() {
+async function createApp() {
   if (!app) {
+    // Dynamically import NestJS after build
+    const { NestFactory } = require('@nestjs/core');
+    const { AppModule } = require('../backend/dist/app.module');
+    
     const expressApp = express();
     const nestApp = await NestFactory.create(AppModule, expressApp);
+    
     nestApp.setGlobalPrefix('api');
     nestApp.enableCors();
     await nestApp.init();
+    
     app = expressApp;
   }
   return app;
 }
 
 module.exports = async (req, res) => {
-  const server = await bootstrap();
+  const server = await createApp();
   return server(req, res);
 };
