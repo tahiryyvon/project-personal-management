552abeb71ecf5f9600668046f3f615d46e802b00
tahiryyvon <tahiry.yvon@gmail.com>
2025-10-31 16:39:02 +0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,8 +2,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-// This is the only thing Vercel needs.
-// It's a standard handler that bootstraps the NestJS app.
 export default async function handler(req, res) {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
